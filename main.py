aaff40cf15674390e5b7cadfa6abce82aa86e020
Suyash L <suyashblal@gmail.com>
2021-03-24 19:52:44 +0000

    Something

--- main.py
+++ main.py
@@ -12,3 +12,32 @@ def fibonaci(n):
     b = c
 
 fibonaci(100)
+
+
+
+
+
+#A list is an ordered collection of elements.
+# List is a collection which is ordered and changeable.
+# Allows duplicate members.
+# These elements can be any type of data.
+
+
+
+print("Welcome to python programing!")
+print("Today  we will be learning  list data structure in deatails")
+numbers = [12,100,0,1,5,23,25,26,101-1]
+print("Original List of Numbers",numbers)
+
+print(str(max(list)))
+
+print(str(min(list)))
+
+list.extend([100,200,300,400])
+print(list)
+
+list.append(200)
+
+print(list)
+
+list.sort()
